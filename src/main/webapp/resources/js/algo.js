9b68c65b0f55ff721b8058dab1d1db1a1cbd05db
kimjintae <wlsxo2112>
2018-09-13 20:50:35 +0900

    ajax ing...

--- src/main/webapp/resources/js/algo.js
+++ src/main/webapp/resources/js/algo.js
@@ -2,7 +2,6 @@
 var algo = algo || {};
 algo ={
 		init : x=>{
-		
 			algo.onCreate(x);
 			algo.setContentView();
 		},
@@ -27,22 +26,114 @@ algo.main ={
 					+'</tr>'
 					+'</table>'
 					+'</div>');
-			$('#t_1').html('<a id="arith_seq"><h3>등차수열</h3></a>');
+			let $t_1 = $('#t_1');
+			let $t_r = $('#t_r');
+			$("<ul />").attr({id : 'side_menu'}).addClass('list-group').appendTo($t_1);
+			$('<li/>').attr({id: 'arith'}).addClass('list-group-item').appendTo($('#side_menu'));
+			$('<a/>').attr({href : '#'}).html('등차수열의합').appendTo($('#arith'))
+			.click(e=>{
+				/*let ques = 
+					'<div id="ques"><h3>시작값 x, 마지막값 y, 공차 d 인 등차수열의 합을 구하시오.</h3>'
+					+'	<label for="시작값">시작값</label><input id="sta" type="text" value = ""></br>'
+					+'	<label for="마지막값">마지막값</label><input id="end" type="text" value =""></br>'
+					+'	<label for="공차">공차</label><input id="d" type="text" value=""></br>'
+					+'	<button id="bt">결과보기</button>'
+				    +'	<h6 id= "rs">결과보기</h6></div>'; */
+				$('<div/>').attr({id:'ques'}).appendTo($t_r);
+				$('<h3/>').html('시작값 x, 마지막값 y, 공차 d 인 등차수열의 합을 구하시오.').appendTo($('#ques'));
+				$('<label/>').html('시작값').appendTo($('#ques'));
+				$('<input/>').attr({id:'sta',type:'text'}).appendTo($('#ques'));
+				$('<br/>').appendTo($('#ques'));
+				$('<label/>').html('마지막값').appendTo($('#ques'));
+				$('<input/>').attr({id:'end',type:'text'}).appendTo($('#ques'));
+				$('<br/>').appendTo($('#ques'));
+				$('<label/>').html('공차').appendTo($('#ques'));
+				$('<input/>').attr({id:'d',type:'text'}).appendTo($('#ques'));
+				$('<br/>').appendTo($('#ques'));
+				$('<button/>').addClass('btn').html('결과보기.').appendTo($('#ques'))
+				.click(e=>{
+					$('<h5/>').appendTo($('#ques')).empty().text(($.fn.nullChecker([
+					     $('#sta').val(),
+						 $('#end').val(), 
+						 $('#d').val()
+					])))?
+					'빈칸을 채우세요': '완성';
+					$('#t_r').html(ques);
+						if(rs=='완성'){
+						let sta = $('#sta').val()*1;
+						let end = $('#end').val()*1; 
+						let d = $('#d').val()*1;
+						let i = sta;
+						let sum = 0;
+						console.log(sta+','+end+','+d);
+						while(i<=end){
+							sum += i;
+							i = d + i;
+						}
+					}
+						
+						$('#rs').append('답 : '+sum);
+					
+				});
+		});
+		
+				
+			
+		/*	$('#t_1').html('<a id="arith_seq"><h3>등차수열</h3></a>');
 			$('#t_1').append('<a id="swit_seq"><h3>스위치수열</h3></a>');
 			$('#t_1').append('<a id="fibo_seq"><h3>피보나치수열</h3></a>');
-			$('#t_1').append('<a id="fact_seq"><h3>팩토리얼수열</h3></a>');
+			$('#t_1').append('<a id="fact_seq"><h3>팩토리얼수열</h3></a>'); 
 			$('#arith_seq').click(e=>{
-				alert('등차수열 선택');
+			
 			});
 			$('#swit_seq').click(e=>{
-				alert('피보나치수열 선택');
+				let ques = 
+					'<h3>시작값 x, 마지막값 y, 공차 d 인 스위치수열의 합을 구하시오.</h3>'
+					+'	<label for="시작값">시작값</label><input id="sta" type="text" value = ""></br>'
+					+'	<label for="마지막값">마지막값</label><input id="end" type="text" value =""></br>'
+					+'	<label for="공차">공차</label><input id="d" type="text" value=""></br>'
+					+'	<button id="bt">결과보기</button>';
+				$('#t_r').html(ques);
+				
+				$('#bt').click(()=>{
+					let sta = $('#sta').val()*1;
+					let end = $('#end').val()*1;
+					let d = $('#d').val()*1;
+					let i = sta;
+					let sum = 0;
+					let sw = 0; 
+					while(i<=end){
+						i = d + i;
+						if(sw==0){
+							sum+=i;
+							sw = 1;
+							console.log(i+","+sum);
+						}else{
+							sum-=i;
+							sw = 0;
+							console.log("---"+i+","+sum);
+						}
+					}
+					$('#t_r').append('답 : '+sum);	
+				});
+				
 			});
 			$('#fibo_seq').click(e=>{
-				alert('스위치수열 선택');
+				let ques = 
+					'<h3>시작값 x, 마지막값 y, 공차 d 인 스위치수열의 합을 구하시오.</h3>'
+					+'	<label for="시작값">시작값</label><input id="sta" type="text" value = ""></br>'
+					+'	<label for="마지막값">마지막값</label><input id="end" type="text" value =""></br>'
+					+'	<label for="공차">공차</label><input id="d" type="text" value=""></br>'
+					+'	<button id="bt">결과보기</button>';
+				$('#t_r').html(ques);
+				
+				
 			});
 			$('#fact_seq').click(e=>{
 				alert('팩토리수열 선택');
-			});
+			});*/
+		
+		
 		}
 };
 algo.series = {
@@ -52,7 +143,7 @@ algo.series = {
 		factorial : x => {} //팩토리
 };
 algo.array = {
-		bublle : x=> {},
+		bubble : x=> {},
 		insert : x=> {},
 		select : x=> {},
 		ranking : x=> {}
@@ -71,14 +162,15 @@ algo.appl = {};
 
 algo.router ={
 	onCreate : x =>{
-		
 		$.getScript(x+'/resources/js/router.js',
 				()=>{
 					$.extend(new Session(x));
+					$.getScript($.ctx()+'/resources/js/util.js')
+					.done(x=>{console.log('실행');})
+					.fail(x=>{console.log('실패')});
 					algo.main.onCreate();
 					
 				}
 		);
 	}
 };
-
